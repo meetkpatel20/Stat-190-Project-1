76181e0bae2804066e5ca99c03bf303e17bc3330
meetkpatel20 <71524817+meetkpatel20@users.noreply.github.com>
2023-02-19 16:41:14 -0600

    Update python_mergefiles.py

--- csv_merger/python_mergefiles.py
+++ csv_merger/python_mergefiles.py
@@ -14,3 +14,35 @@ frame = pd.concat(list, axis=0, ignore_index=True)
 frame.to_csv('merged_csv.csv', index=False, header=False)
 
 print(frame)
+
+#-----------------------------------------------------------------------------------------------------------#
+#New tweaked code for merging
+
+
+
+def is_non_zero_file(fpath):  
+    return os.path.isfile(fpath) and os.path.getsize(fpath) > 0
+
+import pandas as pd
+import glob
+import os
+import numpy as np
+
+directoryPath = '/content/drive/MyDrive/STAT 190 data/Gearbox Oil Temperature/' # use your path
+
+datalist = []
+
+files = glob.glob(directoryPath+'*.csv')
+
+
+for file_name in glob.glob(directoryPath+'*.csv'):
+  if is_non_zero_file(file_name):
+      temp_df = pd.read_csv(file_name)
+      df = pd.DataFrame(temp_df)
+      df.columns =['Turbine', 'Error Time', 'Error Date', 'Temp', 'Error Type']
+      datalist.append(df)
+
+frame = pd.concat(datalist, axis=0, ignore_index=True)
+frame.to_csv('/content/drive/MyDrive/STAT 190 data/All_oil_temp_merged.csv', index=False, header=True)
+
+#print(frame)
